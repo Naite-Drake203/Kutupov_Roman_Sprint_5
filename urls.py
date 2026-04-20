09980b8b802bb2f8619b544d2e1cf8d097000dda
Naite-Drake203 <-email@example.com>
2026-04-20 17:07:26 +0300

    Clean main: keep only README and .gitignore

--- urls.py
+++
@@ -1,6 +0,0 @@
-class URLS:
-    MAIN_PAGE_URL = 'https://stellarburgers.nomoreparties.site/' #URL главной страницы
-    AUTH_PAGE_URL = 'https://stellarburgers.nomoreparties.site/login' #URL страницы авторизации
-    REG_PAGE_URL = 'https://stellarburgers.nomoreparties.site/register' #URL страницы регистрации
-    RECOVER_PAGE_URL = 'https://stellarburgers.nomoreparties.site/forgot-password' #URL страницы восстановления пароля
-    PROFILE_PAGE_URL = 'https://stellarburgers.nomoreparties.site/account/profile' #URL страницы личного кабинета
